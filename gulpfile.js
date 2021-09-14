449959f37772d2be6fdaf9e1767022911006634d
hollywoodpig <nekitkld@gmail.com>
2021-09-14 21:00:22 +0200

    2.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp')
+const ssi = require('ssi')
 const browserSync = require('browser-sync').create()
+const browserSyncSsi = require('browsersync-ssi')
 const concat = require('gulp-concat')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify-es').default
 const sass = require('gulp-sass')(require('sass'))
+const sassGlob = require('gulp-sass-glob')
 const autoprefixer = require('gulp-autoprefixer')
 const imagemin = require('gulp-imagemin')
 const fs = require('fs')
@@ -11,11 +14,12 @@ const fs = require('fs')
 const browserSyncing = () => {
   browserSync.init({
     server: {baseDir: 'app'},
+    middleware: browserSyncSsi({baseDir: 'app', ext: '.html'}),
     notify: false
   })
 }
 
-const stylesVendors = () => {
+const styleVendors = () => {
   return gulp.src('app/sass/vendors.sass')
   .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError) )
   .pipe(concat('vendors.min.css'))
@@ -25,6 +29,7 @@ const stylesVendors = () => {
 
 const styles = () => {
   return gulp.src('app/sass/app.sass')
+  .pipe(sassGlob())
   .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError) )
   .pipe(concat('app.min.css'))
   .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
@@ -32,7 +37,7 @@ const styles = () => {
   .pipe(browserSync.stream())
 }
 
-const scriptsVendors = () => {
+const scriptVendors = () => {
   return gulp.src([
     'node_modules/jquery/dist/jquery.js'
   ])
@@ -73,12 +78,20 @@ const buildCopy = () => {
   .pipe(gulp.dest('dist'))
 }
 
+const buildHtml = async () => {
+  const includes = new ssi('app', 'dist', '**/*.html')
+
+  includes.compile()
+
+  return fs.rmdirSync('dist/parts', {recursive: true})
+}
+
 const watching = () => {
   gulp.watch(['app/sass/*.sass', '!app/sass/vendors.sass'], styles)
-  gulp.watch(['app/sass/vendors.sass'], stylesVendors)
+  gulp.watch(['app/sass/vendors.sass'], styleVendors)
   gulp.watch(['app/js/*.js', '!app/js/*.min.js'], scripts)
   gulp.watch('app/**/*.html').on('change', browserSync.reload)
 }
 
-exports.build = gulp.series(removeDist, stylesVendors, styles, scriptsVendors, scripts, buildCopy, buildImages)
-exports.default = gulp.parallel(stylesVendors, styles, scriptsVendors, scripts, browserSyncing, watching)
+exports.build = gulp.series(removeDist, styleVendors, styles, scriptVendors, scripts, buildCopy, buildHtml, buildImages)
+exports.default = gulp.parallel(styleVendors, styles, scriptVendors, scripts, browserSyncing, watching)
